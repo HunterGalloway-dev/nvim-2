a9ae53fdb15dbc0d9da2224812891dee590b8272
Hunter Galloway <huntergallowaydev@gmail.com>
2025-11-01 17:17:05 -0400

    Adding initial setup for x-ray/go for GoLang support

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -61,4 +61,19 @@ return {
             { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
         },
     },
+    {
+        "ray-x/go.nvim",
+        dependencies = { -- optional packages
+            "ray-x/guihua.lua",
+            "neovim/nvim-lspconfig",
+            "nvim-treesitter/nvim-treesitter",
+        },
+        opts = {
+            -- lsp_keymaps = false,
+            -- other options
+        },
+        event = { "CmdlineEnter" },
+        ft = { "go", "gomod" },
+        build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+    },
 }
