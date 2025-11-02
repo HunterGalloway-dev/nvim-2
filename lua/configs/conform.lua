3df8a7656b95a89b96a13064d3d5d30f3aa8b11e
Hunter Galloway <huntergallowaydev@gmail.com>
2025-11-02 06:40:27 -0500

    Adding prettier for json and yaml auto formatters

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,18 +1,20 @@
 local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    go = { "gofumpt", "goimports" },
-    gomod = { "gofumpt" },
-    gowork = { "gofumpt" },
-    gosum = { "gosum" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
-  },
+    formatters_by_ft = {
+        lua = { "stylua" },
+        go = { "gofumpt", "goimports" },
+        gomod = { "gofumpt" },
+        gowork = { "gofumpt" },
+        gosum = { "gosum" },
+        -- css = { "prettier" },
+        -- html = { "prettier" },
+        json = { "prettier" },
+        yaml = { "prettier" },
+    },
 
-  -- Apply format-on-save globally
-  format_on_save = function()
-    return { timeout_ms = 2000, lsp_fallback = true }
-  end,
+    -- Apply format-on-save globally
+    format_on_save = function()
+        return { timeout_ms = 2000, lsp_fallback = true }
+    end,
 }
 
 return options
